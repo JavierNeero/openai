6716456ebc0af356296729ead9c047678659c597
Javier Polo <jjpolo@gmail.com>
2023-11-10 12:12:47 -0500

    version docker aws ecs

---
+++ README.md
@@ -0,0 +1,19 @@
+# Asistente de Investigación de Mercados
+
+Bienvenido al Asistente de Investigación de Mercados, especializado en Net Promoter Score (NPS) y estudios de satisfacción y lealtad del cliente. Nuestro objetivo es capturar opiniones honestas y detalladas de los clientes para mejorar la experiencia del cliente.
+
+## Encuesta NPS Mejorada
+
+1. **Pregunta Obligatoria**: ¡Valoramos tu opinión! En una escala de 0 a 10, ¿cuán probable es que nos recomiendes a tus conocidos? (0 = nada probable, 10 = extremadamente probable) 👪
+
+2. **Profundización General**: Gracias por tu calificación. ¿Podrías contarnos qué fue lo que más influyó en tu decisión? Así nos ayudas a mejorar. 🤔
+
+3. **Para Promotores (9-10)**: ¡Estamos encantados de contar con tu aprobación! Cuéntanos, ¿qué aspecto de nuestro servicio te impresionó más? Esto nos ayuda a mantener lo que amas. 🌟
+
+4. **Para Detractores (0-6)**: Sentimos no haber estado a la altura de tus expectativas. Tu feedback es crucial: ¿qué deberíamos cambiar para ofrecerte una mejor experiencia? 🚩
+
+5. **Para Pasivos (7-8)**: Tu opinión es muy valiosa para nosotros. ¿Qué crees que podemos hacer para que tu próxima experiencia sea excepcional? 🛠️
+
+6. **Cierre con Acción**: Tu tiempo y tus comentarios son muy apreciados. ¿Hay algo más que te gustaría añadir o algún comentario adicional que quisieras compartir? 🤗
+
+7. **Agradecimiento**: ¡Muchas gracias por ayudarnos con tus comentarios! Trabajaremos para ser aún mejores para ti. ¡Que tengas un día estupendo! 🍀
---
+++ app.py
@@ -0,0 +1,114 @@
+import os
+from openai import AsyncOpenAI
+from openai.types.beta.threads import (
+    MessageContentImageFile,
+    MessageContentText,
+    ThreadMessage,
+)
+import chainlit as cl
+
+
+api_key = os.environ.get("OPENAI_API_KEY")
+org_id = os.environ.get("ORG_ID")
+client = AsyncOpenAI(organization=org_id, api_key=api_key)
+assistant_id = os.environ.get("ASSISTANT_ID")
+
+
+async def process_thread_message(thread_message: ThreadMessage):
+    for content_message in thread_message.content:
+        if isinstance(content_message, MessageContentText):
+            await cl.Message(
+                author=thread_message.role, content=content_message.text.value
+            ).send()
+        elif isinstance(content_message, MessageContentImageFile):
+            image_id = content_message.image_file.file_id
+            response = await client.files.with_raw_response.retrieve_content(image_id)
+            await cl.Message(
+                author=thread_message.role,
+                content="",
+                elements=[
+                    cl.Image(
+                        name=image_id,
+                        content=response.content,
+                        display="inline",
+                        size="large",
+                    ),
+                ],
+            ).send()
+
+
+@cl.on_chat_start
+async def start_chat():
+    thread = await client.beta.threads.create()
+    cl.user_session.set("thread", thread)
+    await cl.Message(author="assistant", content="¡Hola!, ¿Cómo estas?").send()
+
+
+@cl.on_message
+async def run_conversation(message_from_ui: cl.Message):
+    thread = cl.user_session.get("thread")
+    # Add the message to the thread
+    init_message = await client.beta.threads.messages.create(
+        thread_id=thread.id, role="user", content=message_from_ui.content
+    )
+
+    # Create the run
+    run = await client.beta.threads.runs.create(
+        thread_id=thread.id, assistant_id=assistant_id
+    )
+
+    # Send empty message to display the loader
+    loader_msg = cl.Message(author="assistant", content="")
+    await loader_msg.send()
+
+    running = True
+
+    # Wait for the run to be completed
+    while running:
+        run = await client.beta.threads.runs.retrieve(
+            thread_id=thread.id, run_id=run.id
+        )
+        if run.status in ["cancelled", "failed", "completed", "expired"]:
+            break
+        await cl.sleep(0.2)
+
+    # Fetch the run steps
+    run_steps = await client.beta.threads.runs.steps.list(
+        thread_id=thread.id, run_id=run.id, order="asc"
+    )
+
+    for step in run_steps.data:
+        # Fetch step details
+        run_step = await client.beta.threads.runs.steps.retrieve(
+            thread_id=thread.id, run_id=run.id, step_id=step.id
+        )
+        step_details = run_step.step_details
+        # Send step content to the Chainlit UI
+        if step_details.type == "message_creation":
+            thread_message = await client.beta.threads.messages.retrieve(
+                message_id=step_details.message_creation.message_id,
+                thread_id=thread.id,
+            )
+            await process_thread_message(thread_message)
+        if step_details.type == "tool_calls":
+            for tool_call in step_details.tool_calls:
+                if tool_call.type == "code_interpreter":
+                    await cl.Message(
+                        author=tool_call.type,
+                        content=tool_call.code_interpreter.input,
+                        language="python",
+                        parent_id=message_from_ui.id,
+                    ).send()
+                    await cl.Message(
+                        author=f"{tool_call.type}_result",
+                        content=tool_call.code_interpreter.outputs,
+                        parent_id=message_from_ui.id,
+                    ).send()
+                elif tool_call.type == "retrieval":
+                    await cl.Message(
+                        author=tool_call.type,
+                        content="Retrieving information",
+                        parent_id=message_from_ui.id,
+                    ).send()
+
+    await loader_msg.remove()
